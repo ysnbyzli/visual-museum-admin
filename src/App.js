1ad557d86477621fb2b73c0890867faf3344cbd5
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2022-05-29 11:02:42 +0300

    feat: collapsible sidebar 👌🏻

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import Container from "./components/layout/Container";
 import Sidebar from "./components/navigation/Sidebar";
 import { Layout } from "antd";
+import { useState } from "react";
 const { Content, Sider } = Layout;
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
   return (
     <div className="flex h-full">
       <BrowserRouter>
@@ -19,10 +21,16 @@ function App() {
               top: 0,
               bottom: 0,
             }}
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(value) => setCollapsed(value)}
           >
             <Sidebar />
           </Sider>
-          <Layout className="site-layout" style={{ marginLeft: 200 }}>
+          <Layout
+            className="site-layout"
+            style={{ marginLeft: collapsed ? 50 : 200 }}
+          >
             <Content
               style={{
                 margin: "24px 16px 0",
